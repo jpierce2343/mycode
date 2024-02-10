8489938e518ebe64c73e043328338ae58ae82f83
jpierce2343 <jonathanpierce76@gmail.com>
2024-02-10 03:44:42 +0000

    Learning to troubleshoot

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -5,22 +5,19 @@
 calc1 = 0.0
 calc2 = 0.0
 operation = ""
-while (calc1 != "q"):
+while calc1 != "q":
     print("\nWhat is the first operator? Or, enter q to quit: ")
     calc1 = input()
-    if calc1 == "q":
+    if calc1.lower() == "q":
         break
-    if calc2.isdigit() or (calc2[0] == '-' and calc2[1:].isdigit()):
-        calc2 = float(calc2)
-    else:
-        print("/n
+    calc1 = float(calc1)
     print("\nWhat is the second operator? Or, enter q to quit: ")
     calc2 = input()
-    if calc2 == "q":
+    if calc2.lower() == "q":
         break
     calc2 = float(calc2)
     print("Enter an operation to perform on the two operators (+ or -): ")
-    operation = raw_input()
+    operation = input()
     if operation == "+":
         print("\n" + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
     elif operation == '-':
