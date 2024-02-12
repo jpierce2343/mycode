6271172300dbc27da27c8280ade51d8495841749
jpierce2343 <jonathanpierce76@gmail.com>
2024-02-12 23:35:31 +0000

    python_project

---
+++ color/color3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import crayons
+
+def main():
+    """run time code. Always indent under function"""
+
+    print("{crayons.red('Pierce'),{crayons.blue{'Jon'})
+
+if __name__ == "__main__":
+    main()
--- python_project.py
+++ python_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3         ## python_project.py
 ## Dictionaries set for user input
+import crayons
 weapon= ["daniel defense", "sig", "bcm", "knight's armament"]
 
 a= weapon[0]
@@ -19,27 +20,26 @@ muzzle= ["suppressed", "flash hider"]
 a= muzzle[0]
 b= muzzle[1]
 
+def main():
 # Choose first item
-print("choose a weapon (a, b, c, or d)")
-choice = input()
-weapon_choice = weapon[ord(choice) - ord('a')]
+    print("choose a weapon (a, b, c, or d)")
+    choice = input()
+    weapon_choice = weapon[ord(choice) - ord('a')]
 
 #choose second item
-print("choose sights (a, b, c, or d)")
-choice = input()
-optic_choice = optic[ord(choice) - ord('a')]
+    print("choose sights (a, b, c, or d)")
+    choice = input()
+    optic_choice = optic[ord(choice) - ord('a')]
 
 # Choose third item
-print("choose barrel end (a or b)")
-choice = input()
-muzzle_choice = muzzle[ord(choice) - ord('a')]
+    print("choose barrel end (a or b)")
+    choice = input()
+    muzzle_choice = muzzle[ord(choice) - ord('a')]
 
 # Print out rifle combination
-print(f"Your rifle is {weapon_choice} with {optic_choice} and {muzzle_choice}")
+    print(f"{crayons.red('Your rifle is {weapon_choice} with {optic_choice} and {muzzle_choice}')}")
 
 
 
-
-print(f"your rifle is (weapon[input]) with (optic[imput]) and (muzzle[input])")
 
 main()
